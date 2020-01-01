fc9a6a3cbee2148a2a8ce42455966f93aaa86f70
 bkozarik <bkozarik@iMac-bkozarik.local>
2020-01-01 23:33:08 +0200

    Add files

---
+++ assets/js/main.js
@@ -0,0 +1,3 @@
+// let tab__item = document.querySelector('.tab__item'),
+//     active__tab = document.querySelector('.active__tab'),
+//     active__items = document.querySelector('.active__items');
